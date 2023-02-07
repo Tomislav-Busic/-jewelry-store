809ee2a2c3317680d04d585b37b8cd8eaef0110c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-07 18:24:47 +0100

    home navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Home } from "./Pages/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
