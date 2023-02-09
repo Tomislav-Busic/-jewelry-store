8aafcdd36f031556882c88ecf9bda1f9eec27fa8
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-09 19:33:36 +0100

    admin dashboard

---
+++ src/Pages/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./AdminDashboard.scss";
+
+export const AdminDashboard = () => {
+  return (
+    <div className="admin-dashboard">
+      <h1>Admin Dashboard</h1>
+      <div className="container">
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  );
+};
