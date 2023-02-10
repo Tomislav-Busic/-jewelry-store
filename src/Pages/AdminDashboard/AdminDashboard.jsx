e7969c2a131e5381768e9d54c9c6a0c8b8ac3210
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-10 20:16:56 +0100

    admin dashboard style

--- src/Pages/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/AdminDashboard/AdminDashboard.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import "./AdminDashboard.scss";
 
 export const AdminDashboard = () => {
+  const handleSubmit = () => {};
+
   return (
     <div className="admin-dashboard">
       <h1>Admin Dashboard</h1>
       <div className="container">
-        <form>
-          <input type="text" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" id="name" placeholder="Ime proizvoda" />
+          <input type="number" id="price" placeholder="Cijena proizvoda" />
+          <select id="category">
+            <option value="ostalo">Ostalo (kategorija)</option>
+            <option value="satovi">Satovi</option>
+            <option value="inventar">Inventar</option>
+            <option value="slike">Slike</option>
+          </select>
+          <label>Opis proizvoda</label>
+          <textarea id="description" placeholder="Opis proizvoda" />
+          <button type="submit">Potvrdi</button>
         </form>
       </div>
     </div>
