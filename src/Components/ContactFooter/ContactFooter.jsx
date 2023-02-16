746f4bd4a54c1a870620867e6e26d1ce1e320dbd
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-16 19:06:18 +0100

    links

--- src/Components/ContactFooter/ContactFooter.jsx
+++ src/Components/ContactFooter/ContactFooter.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./ContactFooter.scss";
 import { IoMail, IoCall } from "react-icons/io5";
-import { FaUser, FaKey, FaLink, FaMapMarkerAlt } from "react-icons/fa";
+import {
+  FaUser,
+  FaKey,
+  FaLink,
+  FaMapMarkerAlt,
+  FaWarehouse,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export const ContactFooter = () => {
@@ -68,6 +74,10 @@ export const ContactFooter = () => {
           <ul>
             <h2>Calvi antique</h2>
             <li>
+              <FaWarehouse className="icon" />
+              <p className="link">CALVI ANTIQUE j.d.o.o.</p>
+            </li>
+            <li>
               <FaUser className="icon" />
               <p className="link">Anita Calvi</p>
             </li>
@@ -77,11 +87,35 @@ export const ContactFooter = () => {
             </li>
             <li>
               <FaKey className="icon" />
-              <Link to="/login" className="link">
+              <Link to="/admin" className="link">
                 Administrator
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="f-con-item">
+          <ul>
+            <h2>Radno vrijeme</h2>
+            <li>
+              <p className="time">Pon-Pet:</p>
+              <p className="link">8-20h</p>
+            </li>
+            <li>
+              <p className="time">Sub:</p>
+              <p className="link">8-15h</p>
+            </li>
+            <li>
+              <p className="time">Ned:</p>
+              <p className="link">Ne radimo</p>
+            </li>
+          </ul>
+        </div>
+        <div className="f-con-item">
+          <iframe
+            className="map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5561.997434253886!2d15.974978527278504!3d45.81128408400844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765d6fc299fd2e1%3A0xe38d6d13b1fff011!2sPra%C5%A1ka%20ul.%2010%2C%2010000%2C%20Zagreb!5e0!3m2!1sbs!2shr!4v1676570422584!5m2!1sbs!2shr"
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
       <div className="social"></div>
