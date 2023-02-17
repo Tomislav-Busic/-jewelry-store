00fae1048bc56cd269e2014468a83a8c0b8c7785
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-17 22:45:38 +0100

    login slice

---
+++ src/store/slice/login-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState: {
+    isLoggedIn: null,
+  },
+  reducers: {
+    login(state, action) {
+      state.isLoggedIn = action.payload;
+    },
+    logout(state) {
+      state.isLoggedIn = null;
+    },
+  },
+});
+
+export const loginActions = loginSlice.actions;
