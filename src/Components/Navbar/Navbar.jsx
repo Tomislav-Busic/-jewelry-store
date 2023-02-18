34163db145ef07a9962baa178d18ef4d7521e2a6
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-18 13:47:48 +0100

    logo center navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,52 +3,58 @@ import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { goToTopOfPage } from "../../Tools/Tools";
+import logo from "../../images/logo.jpg";
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <header>
-      <h3>CALVI</h3>
-      <nav className={toggle ? "resposive_nav" : ""}>
-        <Link
-          className="nav-link"
-          to="/"
-          onClick={() => (setToggle(!toggle), goToTopOfPage())}
-        >
-          Početna
-        </Link>
-        <Link
-          className="nav-link"
-          to="/prodaja"
-          onClick={() => (setToggle(!toggle), goToTopOfPage())}
-        >
-          Prodaja
-        </Link>
-        <Link
-          className="nav-link"
-          to="/usluge "
-          onClick={() => (setToggle(!toggle), goToTopOfPage())}
-        >
-          Usluge
-        </Link>
-        <Link
-          className="nav-link"
-          to="/kontakt"
-          onClick={() => (setToggle(!toggle), goToTopOfPage())}
-        >
-          Kontakt
-        </Link>
-        <button
-          className="nav-btn nav-close-btn"
-          onClick={() => setToggle(!toggle)}
-        >
-          <FaTimes />
+    <div className="navbar">
+      <header>
+        <h3>CALVI</h3>
+        <nav className={toggle ? "resposive_nav" : ""}>
+          <Link
+            className="nav-link"
+            to="/"
+            onClick={() => (setToggle(!toggle), goToTopOfPage())}
+          >
+            POČETNA
+          </Link>
+          <Link
+            className="nav-link"
+            to="/prodaja"
+            onClick={() => (setToggle(!toggle), goToTopOfPage())}
+          >
+            PRODAJA
+          </Link>
+          <Link
+            className="nav-link"
+            to="/usluge "
+            onClick={() => (setToggle(!toggle), goToTopOfPage())}
+          >
+            USLUGE
+          </Link>
+          <Link
+            className="nav-link"
+            to="/kontakt"
+            onClick={() => (setToggle(!toggle), goToTopOfPage())}
+          >
+            KONTAKT
+          </Link>
+          <button
+            className="nav-btn nav-close-btn"
+            onClick={() => setToggle(!toggle)}
+          >
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={() => setToggle(!toggle)}>
+          <FaBars />
         </button>
-      </nav>
-      <button className="nav-btn" onClick={() => setToggle(!toggle)}>
-        <FaBars />
-      </button>
-    </header>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </header>
+    </div>
   );
 };
