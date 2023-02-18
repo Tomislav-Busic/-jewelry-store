f056210f9b7fde3bd939ceaa8caf073be1777605
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-18 14:26:25 +0100

    home stuff

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { InfiniteSlider } from "../../Components/InfiniteSlider/InfiniteSlider";
+import {
+  FaFacebookSquare,
+  FaLinkedin,
+  FaInstagramSquare,
+} from "react-icons/fa";
 import "./Home.scss";
 
 export const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <div className="section s-one"></div>
+        <div className="section s-one">
+          <h1>
+            <span>CALVI </span>antique fine jewellery
+          </h1>
+          <h3>Praška 10 Zagreb</h3>
+          <h4>Anita Calvi</h4>
+          <div className="social">
+            <a>
+              <FaFacebookSquare className="social-icon" />
+            </a>
+            <a
+              href="https://www.instagram.com/calviantique_finejewellery/"
+              target="_blank"
+            >
+              <FaInstagramSquare className="social-icon" />
+            </a>
+            <a>
+              <FaLinkedin className="social-icon" />
+            </a>
+          </div>
+          <button>Kontakt</button>
+        </div>
         <InfiniteSlider />
       </div>
     </div>
