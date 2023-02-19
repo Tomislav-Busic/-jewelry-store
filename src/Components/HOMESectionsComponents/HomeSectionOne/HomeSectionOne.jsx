d63029f19b916d6a2403f5e5d1607051fbf4d833
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-19 19:09:12 +0100

    Home section one

---
+++ src/Components/HOMESectionsComponents/HomeSectionOne/HomeSectionOne.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { TextAnimation } from "../../TextAnimation/TextAnimation";
+import { AnimationBtn } from "../../AnimationBtn/AnimationBtn";
+import { SocialIcons } from "../../SocialIcons/SocialIcons";
+import "./HomeSectionOne.scss";
+
+export const HomeSectionOne = () => {
+  return (
+    <div className="home-section-one">
+      <h1>
+        <span>CALVI </span>antique fine jewellery
+      </h1>
+      <TextAnimation />
+      <SocialIcons />
+      <h3>Praška 10, Zagreb</h3>
+      <AnimationBtn name={"Kontakt"} path={"./kontakt"} />
+    </div>
+  );
+};
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,13 @@
 import React from "react";
+import { HomeSectionOne } from "../../Components/HOMESectionsComponents/HomeSectionOne/HomeSectionOne";
 import { InfiniteSlider } from "../../Components/InfiniteSlider/InfiniteSlider";
 import "./Home.scss";
-import { TextAnimation } from "../../Components/TextAnimation/TextAnimation";
-import { SocialIcons } from "../../Components/SocialIcons/SocialIcons";
-import { AnimationBtn } from "../../Components/AnimationBtn/AnimationBtn";
 
 export const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <div className="section s-one">
-          <h1>
-            <span>CALVI </span>antique fine jewellery
-          </h1>
-          <TextAnimation />
-          <SocialIcons />
-          <h3>Praška 10, Zagreb</h3>
-          <AnimationBtn name={"Kontakt"} path={"./kontakt"} />
-        </div>
+        <HomeSectionOne />
         <div className="section s-two">
           <h1>O nama</h1>
         </div>
