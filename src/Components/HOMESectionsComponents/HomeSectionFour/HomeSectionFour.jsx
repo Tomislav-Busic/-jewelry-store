fab6965b1930e38af5f5a7e2485e7758a5842ebd
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-20 19:55:15 +0100

    design

--- src/Components/HOMESectionsComponents/HomeSectionFour/HomeSectionFour.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFour/HomeSectionFour.jsx
@@ -5,7 +5,7 @@ import "./HomeSectionFour.scss";
 export const HomeSectionFour = () => {
   return (
     <div className="home-section-four">
-      <h1>O nama</h1>
+      <h1>Usluge</h1>
       <AnimationBtn name={"Usluge"} path={"./usluge"} color={"btn-anim-gold"} />
     </div>
   );
