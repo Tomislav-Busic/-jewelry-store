80f4a117896d746b3719a599c56a1843898b2eb2
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-21 19:32:28 +0100

    hsf test

--- src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { testemonials } from "./testemonials";
 
 export const HSFiveSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+
   return <div>HSFiveSlider</div>;
 };
---
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/testemonials.js
@@ -0,0 +1,10 @@
+export const testemonials = [
+  { id: 1, content: "Neka pohvala1", name: "Joža M.1" },
+  { id: 2, content: "Neka pohvala2", name: "Joža M.2" },
+  { id: 3, content: "Neka pohvala3", name: "Joža M.3" },
+  { id: 4, content: "Neka pohvala4", name: "Joža M.4" },
+  { id: 5, content: "Neka pohvala5", name: "Joža M.5" },
+  { id: 6, content: "Neka pohvala6", name: "Joža M.6" },
+  { id: 7, content: "Neka pohvala7", name: "Joža M.7" },
+  { id: 8, content: "Neka pohvala8", name: "Joža M.8" },
+];
