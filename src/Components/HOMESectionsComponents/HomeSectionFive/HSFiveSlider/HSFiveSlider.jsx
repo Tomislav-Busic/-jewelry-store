da2d2ed3ebbfb3a6553b69b361561ae5d2b2d614
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-21 19:28:02 +0100

    hsf slider

--- src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const HSFiveSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   return <div>HSFiveSlider</div>;
 };
