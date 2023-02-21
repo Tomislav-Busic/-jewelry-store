5f35dfab347acd427cb43ac456f903f97ff4077f
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-21 19:37:18 +0100

    hsf slider

--- src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
@@ -4,5 +4,14 @@ import { testemonials } from "./testemonials";
 export const HSFiveSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  return <div>HSFiveSlider</div>;
+  return (
+    <div className="slider">
+      <button>left</button>
+      <button>right</button>
+      <div className="slider-card">
+        <h4>Text</h4>
+        <p>Name</p>
+      </div>
+    </div>
+  );
 };
