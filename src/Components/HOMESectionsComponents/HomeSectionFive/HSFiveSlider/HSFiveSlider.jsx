b327621c311af5f7e6cabbd3da6eae4c44ef7f99
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-22 15:17:18 +0100

    slider-testemonials

--- src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { testemonials } from "./testemonials";
 import "./HSFiveSlider.scss";
+import { testemonials } from "./testemonials";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 export const HSFiveSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <div className="slider-testem">
-      <button className="btn-st left">left</button>
-      <button className="btn-st right">right</button>
+      <button className="btn-st left">
+        <FaChevronLeft className="icon" />
+      </button>
+      <button className="btn-st right">
+        <FaChevronRight className="icon" />
+      </button>
       <div className="slider-content">
         <h4>Text</h4>
         <p>Name</p>
