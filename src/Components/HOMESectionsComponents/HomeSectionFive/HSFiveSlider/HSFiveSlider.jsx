24116ece79475d46f6435fb83fb861d510109ede
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-22 14:50:13 +0100

    slider-testemonials

--- src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { testemonials } from "./testemonials";
+import "./HSFiveSlider.scss";
 
 export const HSFiveSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <div className="slider">
+    <div className="slider-testem">
       <button>left</button>
       <button>right</button>
       <div className="slider-card">
--- src/Components/HOMESectionsComponents/HomeSectionFive/HomeSectionFive.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HomeSectionFive.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./HomeSectionFive.scss";
 import { AnimationBtn } from "../../AnimationBtn/AnimationBtn";
+import { HSFiveSlider } from "./HSFiveSlider/HSFiveSlider";
 
 export const HomeSectionFive = () => {
   return (
     <div className="home-section-five">
       <h1>Preporuke</h1>
-
+      <HSFiveSlider />
       <AnimationBtn
         name={"Kontakt"}
         path={"./kontakt"}
