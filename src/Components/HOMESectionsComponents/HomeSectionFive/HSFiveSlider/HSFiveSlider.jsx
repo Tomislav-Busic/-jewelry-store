d378bd59772a6325c50c309f0af4a737d4349ff3
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-22 15:41:50 +0100

    slider-testemonials logic javascript

--- src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFive/HSFiveSlider/HSFiveSlider.jsx
@@ -6,20 +6,32 @@ import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 export const HSFiveSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? testemonials.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isFirstSlide = currentIndex === testemonials.length - 1;
+    const newIndex = isFirstSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <div className="slider-testem">
-      <button className="btn-st left">
+      <button className="btn-st left" onClick={goToPrevious}>
         <FaChevronLeft className="icon" />
       </button>
-      <button className="btn-st right">
+      <button className="btn-st right" onClick={goToNext}>
         <FaChevronRight className="icon" />
       </button>
       <div className="slider-content">
         <h4>
-          <i>Text</i>
+          <i>{testemonials[currentIndex].content}</i>
         </h4>
         <p>
-          <i>Name</i>
+          <i>{testemonials[currentIndex].name}</i>
         </p>
       </div>
     </div>
