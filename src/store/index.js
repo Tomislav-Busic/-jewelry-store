d5c3103a5597d8cf4bbbc076862f983efe6c60a0
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-22 16:40:57 +0100

    import dataSlice in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { loginSlice } from "./slice/login-slice";
+import { dataSlice } from "./slice/data-slice";
 
 export const store = configureStore({
   reducer: {
     login: loginSlice.reducer,
+    data: dataSlice.reducer,
   },
 });
