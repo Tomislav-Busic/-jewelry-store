7f0d377af31d824689f1d3482b548f419e17ab15
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-23 21:39:16 +0100

    AdminPostProductForm

---
+++ src/Components/ADMINComponents/AdminPostProductForm/AdminPostProductForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./AdminPostProductForm.scss";
+import { MdFileUpload } from "react-icons/md";
+
+export const AdminPostProductForm = ({
+  handleSubmit,
+  handleData,
+  setFile,
+  file,
+  perc,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Unesite novi proizvod</h3>
+      <input
+        type="text"
+        id="name"
+        placeholder="Ime proizvoda"
+        onChange={handleData}
+      />
+      <input
+        type="number"
+        id="price"
+        placeholder="Cijena proizvoda u €"
+        onChange={handleData}
+      />
+      <select id="category" onChange={handleData}>
+        <option value="ostalo">Odaberite kategoriju </option>
+        <option value="ostalo">Ostalo </option>
+        <option value="satovi">Satovi</option>
+        <option value="inventar">Inventar</option>
+        <option value="slike">Slike</option>
+      </select>
+      <textarea
+        id="description"
+        placeholder="Opis proizvoda"
+        onChange={handleData}
+      />
+      <img
+        src={
+          file
+            ? URL.createObjectURL(file)
+            : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+        }
+        alt=""
+      />
+      <label htmlFor="file">
+        Učitaj sliku: <br />
+        <MdFileUpload className="icon" />
+      </label>
+      <input
+        type="file"
+        id="file"
+        onChange={(e) => setFile(e.target.files[0])}
+        style={{ display: "none" }}
+      />
+      <button disabled={perc !== null && perc < 100} type="submit">
+        Potvrdi
+      </button>
+    </form>
+  );
+};
--- src/Pages/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/AdminDashboard/AdminDashboard.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./AdminDashboard.scss";
-import { MdFileUpload } from "react-icons/md";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { ListOfProductsAdmin } from "../../Components/ADMINComponents/ListOfProductsAdmin/ListOfProductsAdmin";
 
 import { db, storage } from "../../firebase";
+import { AdminPostProductForm } from "../../Components/ADMINComponents/AdminPostProductForm/AdminPostProductForm";
 
 export const AdminDashboard = () => {
   const [data, setData] = useState({});
@@ -52,7 +52,6 @@ export const AdminDashboard = () => {
     file && uploadFile();
   }, [file]);
 
-
   const handleData = (e) => {
     const id = e.target.id;
     const value = e.target.value;
@@ -79,54 +78,13 @@ export const AdminDashboard = () => {
     <div className="admin-dashboard">
       <h1>Administrator</h1>
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          <h3>Unesite novi proizvod</h3>
-          <input
-            type="text"
-            id="name"
-            placeholder="Ime proizvoda"
-            onChange={handleData}
-          />
-          <input
-            type="number"
-            id="price"
-            placeholder="Cijena proizvoda u €"
-            onChange={handleData}
-          />
-          <select id="category" onChange={handleData}>
-            <option value="ostalo">Odaberite kategoriju </option>
-            <option value="ostalo">Ostalo </option>
-            <option value="satovi">Satovi</option>
-            <option value="inventar">Inventar</option>
-            <option value="slike">Slike</option>
-          </select>
-          <textarea
-            id="description"
-            placeholder="Opis proizvoda"
-            onChange={handleData}
-          />
-          <img
-            src={
-              file
-                ? URL.createObjectURL(file)
-                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-            }
-            alt=""
-          />
-          <label htmlFor="file">
-            Učitaj sliku: <br />
-            <MdFileUpload className="icon" />
-          </label>
-          <input
-            type="file"
-            id="file"
-            onChange={(e) => setFile(e.target.files[0])}
-            style={{ display: "none" }}
-          />
-          <button disabled={perc !== null && perc < 100} type="submit">
-            Potvrdi
-          </button>
-        </form>
+        <AdminPostProductForm
+          handleSubmit={handleSubmit}
+          handleData={handleData}
+          setFile={setFile}
+          file={file}
+          perc={perc}
+        />
       </div>
       <ListOfProductsAdmin />
     </div>
