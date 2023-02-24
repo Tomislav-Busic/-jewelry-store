538fb792deb5f1e628795f12dae1fba8a36f3ac9
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-24 14:04:33 +0100

    home sec-4 card images

--- src/Components/HOMESectionsComponents/HomeSectionFour/HomeSecFourCard/HomeSecFourCard.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFour/HomeSecFourCard/HomeSecFourCard.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomeSecFourCard.scss";
 
-export const HomeSecFourCard = ({ img, title }) => {
+export const HomeSecFourCard = ({ title, images }) => {
+  const [currentImg, setCurrentImg] = useState(images[1]);
+  const [style, setStyle] = useState(0);
+
+  const changeImage = (img) => {
+    setCurrentImg(img);
+  };
+
   return (
-    <div
-      className="hsf-card"
-      style={{
-        background: `url(${img})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "50%",
-      }}
-    >
+    <div className="hsf-card">
+      <img src={currentImg} alt={currentImg} />
+      <div className="images-card">
+        {images.map((img, index) => (
+          <img
+            src={img}
+            key={index}
+            alt={img}
+            onClick={() => changeImage(img)}
+          />
+        ))}
+      </div>
       <div className="card-text">
         <h2>{title}</h2>
       </div>
--- src/Components/HOMESectionsComponents/HomeSectionFour/HomeSectionFour.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFour/HomeSectionFour.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import "./HomeSectionFour.scss";
 import img1 from "../../../images/prstend.jpg";
-import img2 from "../../../images/rucprsten.jpg";
-import img3 from "../../../images/prstensmar.jpg";
+import img2 from "../../../images/goldring.jpg";
+import img3 from "../../../images/threerings.jpg";
+import img4 from "../../../images/rucprsten.jpg";
+import img5 from "../../../images/work6.jpg";
+import img6 from "../../../images/work4.jpg";
+import img7 from "../../../images/frogrlice.jpg";
+import img8 from "../../../images/goldeggs.jpg";
+import img9 from "../../../images/antikviteti.jpg";
 import { AnimationBtn } from "../../AnimationBtn/AnimationBtn";
 import { HomeSecFourCard } from "./HomeSecFourCard/HomeSecFourCard";
 
 export const HomeSectionFour = () => {
+  const imagesMakingOf = [img1, img2, img3];
+  const imagesRepairs = [img4, img5, img6];
+  const imagesAntique = [img7, img8, img9];
   return (
     <div className="home-section-four">
       <h1>Usluge</h1>
       <div className="hsf-images">
-        <HomeSecFourCard img={img1} title={"Izrada nakita po narudžbi"} />
-        <HomeSecFourCard img={img2} title={"Popravci i restauriranje nakita"} />
-        <HomeSecFourCard img={img3} title={"Francuski Antikviteti"} />
+        <HomeSecFourCard
+          images={imagesMakingOf}
+          title={"Izrada nakita po narudžbi"}
+        />
+        <HomeSecFourCard
+          images={imagesRepairs}
+          title={"Popravci i restauriranje nakita"}
+        />
+        <HomeSecFourCard
+          images={imagesAntique}
+          title={"Francuski Antikviteti"}
+        />
       </div>
       <AnimationBtn name={"Usluge"} path={"./usluge"} color={"btn-anim-gold"} />
     </div>
