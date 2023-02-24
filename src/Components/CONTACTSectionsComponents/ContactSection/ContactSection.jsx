efe92b287bf4a232687871dcf6dcf6e75aa8c666
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-24 14:44:43 +0100

    height

--- src/Components/CONTACTSectionsComponents/ContactSection/ContactSection.jsx
+++ src/Components/CONTACTSectionsComponents/ContactSection/ContactSection.jsx
@@ -3,7 +3,7 @@ import { InfiniteSlider } from "../../InfiniteSlider/InfiniteSlider";
 
 export const ContactSection = () => {
   return (
-    <div className="home-section-three">
+    <div className="contact-section">
       <h1>Kontakt</h1>
       <InfiniteSlider />
     </div>
