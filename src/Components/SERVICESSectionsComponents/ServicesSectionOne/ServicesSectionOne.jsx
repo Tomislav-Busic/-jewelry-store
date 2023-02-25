96287d9610f5669560bd9052d36c953c5fbdea92
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-25 15:15:33 +0100

    font

--- src/Components/SERVICESSectionsComponents/ServicesSectionOne/ServicesSectionOne.jsx
+++ src/Components/SERVICESSectionsComponents/ServicesSectionOne/ServicesSectionOne.jsx
@@ -5,7 +5,20 @@ import img from "../../../images/work6.jpg";
 export const ServicesSectionOne = () => {
   return (
     <div className="services-s-one">
-      <div className="left-s-side">sdfgdsd</div>
+      <div className="left-s-side">
+        <h2>Usluge</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
       <div className="right-s-side">
         <img src={img} />
       </div>
