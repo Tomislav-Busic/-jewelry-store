6901d996cfd7032d234f7c255f04827b5a8034ae
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-25 14:14:55 +0100

    contact

--- src/Components/CONTACTSectionsComponents/ContactSection/ContactSection.jsx
+++ src/Components/CONTACTSectionsComponents/ContactSection/ContactSection.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import "./ContactSection.scss";
-import { AnimationBtn } from "../../AnimationBtn/AnimationBtn";
+import emailjs from "emailjs-com";
 
 export const ContactSection = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    /* emailjs
+      .sendForm(
+        "service_ybqmmcr",
+        "template_01whte8",
+        e.target,
+        "DSAGwXnCHCTrTMfi3"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      }); */
+  };
+
   return (
     <div className="contact-section">
-      <br />
       <h1>Kontakt</h1>
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button type="submit">Slanje</button>
+      <form onSubmit={sendEmail}>
+        <input type="text" name="name" placeholder="ime" />
+        <input type="email" name="user_email" placeholder="email" />
+        <textarea placeholder="Poruka..." name="message" />
+        <button type="submit" value="Send">
+          Slanje
+        </button>
         <br />
       </form>
     </div>
