ae6cf4379519a57f1212cfe1ae8b795c0d6eaa69
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-26 12:32:17 +0100

    Handle sort by Name

--- src/Components/SALESectionComponents/Products/Products.jsx
+++ src/Components/SALESectionComponents/Products/Products.jsx
@@ -5,12 +5,19 @@ import { ProductCard } from "./ProductCard/ProductCard";
 
 export const Products = () => {
   const products = useSelector((state) => state.data.dataList);
+  const productName = useSelector((state) => state.data.productName);
 
   return (
     <div className="products">
-      {products.map((product, index) => (
-        <ProductCard key={index} product={product} />
-      ))}
+      {products
+        .filter(
+          (product) =>
+            product.name.toLowerCase().includes(productName) ||
+            product.name.includes(productName)
+        )
+        .map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
     </div>
   );
 };
