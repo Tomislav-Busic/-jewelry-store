c48639fcbf0997a8c51a14a2240dfa441c0460fa
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-26 11:10:28 +0100

    ProductCard

--- src/Components/SALESectionComponents/Products/ProductCard/ProductCard.jsx
+++ src/Components/SALESectionComponents/Products/ProductCard/ProductCard.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "./ProductCard.scss";
 
 export const ProductCard = ({ product }) => {
-  const { id, name, description, img, img_name } = product;
-  return <div>ProductCard</div>;
+  const { id, name, description, price, img, img_name } = product;
+  return (
+    <div className="product-card" id={id}>
+      <img src={img} alt={img_name} />
+      <div className="info-card">
+        <h2>{name}</h2>
+        <h2>{price}</h2>
+      </div>
+    </div>
+  );
 };
