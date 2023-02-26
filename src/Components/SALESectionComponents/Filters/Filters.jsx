09e02a988fd188456d54fa804fbda010c974f1bb
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-26 12:22:56 +0100

    Search by Name dispatch

--- src/Components/SALESectionComponents/Filters/Filters.jsx
+++ src/Components/SALESectionComponents/Filters/Filters.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Filters.scss";
+import { useDispatch } from "react-redux";
+import { dataActions } from "../../../store/slice/data-slice";
 
 export const Filters = () => {
+  const dispatch = useDispatch();
+
+  const handleSearchByName = (name) => {
+    dispatch(dataActions.filterByName(name));
+  };
+
   return (
     <div className="filters">
-      <input placeholder="Upiši ime proizvoda" />
+      <input
+        placeholder="Upiši ime proizvoda"
+        onChange={(e) => handleSearchByName(e.target.value)}
+      />
       <select name="Filtriraj po cijeni">
         <option>Sortiraj po cijeni</option>
         <option value="lower">Niža cijena</option>
