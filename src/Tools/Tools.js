0a3529d5fb2651a08ad87244fe6dbf4f2fd9c275
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-26 11:11:22 +0100

    Tools

--- src/Tools/Tools.js
+++ src/Tools/Tools.js
@@ -1,3 +1,12 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: "EUR",
+  style: "currency",
+});
+
+export const FormatCurrency = (number) => {
+  return CURRENCY_FORMATTER.format(number);
+};
+
 export const goToTopOfPage = () => {
   window.scroll({
     top: 0,
