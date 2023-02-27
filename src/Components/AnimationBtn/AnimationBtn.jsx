dd56bc5093fe6b230ac3d8ba03dd32b4b92faf8a
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-27 19:09:09 +0100

    goToTopOfPage

--- src/Components/AnimationBtn/AnimationBtn.jsx
+++ src/Components/AnimationBtn/AnimationBtn.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./AnimationBtn.scss";
+import { goToTopOfPage } from "../../Tools/Tools";
 
 export const AnimationBtn = ({ name, path, color }) => {
   return (
     <div className={color}>
       <div className="button-anim">
-        <Link className="link-anim" to={path}>
+        <Link onClick={goToTopOfPage} className="link-anim" to={path}>
           <span></span>
           <span></span>
           <span></span>
