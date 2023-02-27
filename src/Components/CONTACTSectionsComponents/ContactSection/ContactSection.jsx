116058d3edcaf9869b1b5f3ee93ff76e86705e8f
Tomislav Busic <tomislav.busic89@gmail.com>
2023-02-27 20:24:41 +0100

    contact

--- src/Components/CONTACTSectionsComponents/ContactSection/ContactSection.jsx
+++ src/Components/CONTACTSectionsComponents/ContactSection/ContactSection.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./ContactSection.scss";
 import emailjs from "emailjs-com";
+import { SocialIcons } from "../../SocialIcons/SocialIcons";
+import { IoMail, IoCall } from "react-icons/io5";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 export const ContactSection = () => {
   const sendEmail = (e) => {
@@ -24,15 +27,46 @@ export const ContactSection = () => {
   return (
     <div className="contact-section">
       <h1>Kontakt</h1>
-      <form onSubmit={sendEmail}>
-        <input type="text" name="name" placeholder="ime" />
-        <input type="email" name="user_email" placeholder="email" />
-        <textarea placeholder="Poruka..." name="message" />
-        <button type="submit" value="Send">
-          Slanje
-        </button>
-        <br />
-      </form>
+      <div className="contact-container">
+        <form onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="ime" />
+          <input type="email" name="user_email" placeholder="email" />
+          <textarea placeholder="Poruka..." name="message" />
+          <button type="submit" value="Send">
+            Slanje
+          </button>
+          <br />
+        </form>
+        <div className="contact-links">
+          <SocialIcons />
+          <div className="f-con-item">
+            <ul>
+              <li>
+                <IoMail className="icon" />
+                <a href="mailto:anita@calviantique.hr" className="link">
+                  anita@calviantique.hr
+                </a>
+              </li>
+              <li>
+                <IoCall className="icon" />
+                <a href="tel:+385992882894" className="link">
+                  <span>Anita</span> +385992882894
+                </a>
+              </li>
+              <li>
+                <IoCall className="icon" />
+                <a href="tel:+38598229078" className="link">
+                  <span>Ivica</span> +38598229078
+                </a>
+              </li>
+              <li>
+                <FaMapMarkerAlt className="icon" />
+                <p className="link">Praška 10, Zagreb</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
