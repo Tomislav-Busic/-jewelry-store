f8a5232390d7926c8346dbae0092c2206ae58a6d
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-01 19:55:32 +0100

    animation

--- src/Components/HOMESectionsComponents/HomeSectionFour/HomeSecFourCard/HomeSecFourCard.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionFour/HomeSecFourCard/HomeSecFourCard.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./HomeSecFourCard.scss";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useAnimation } from "framer-motion";
 
 export const HomeSecFourCard = ({ title, images }) => {
   const [currentImg, setCurrentImg] = useState(images[1]);
@@ -8,22 +11,44 @@ export const HomeSecFourCard = ({ title, images }) => {
     setCurrentImg(img);
   };
 
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          durration: 1,
+          bounce: 0.3,
+        },
+      });
+    }
+
+    if (!inView) {
+      animation.start({ x: "-100vw" });
+    }
+  }, [inView]);
+
   return (
-    <div className="hsf-card">
-      <img src={currentImg} alt={currentImg} />
-      <div className="images-card">
-        {images.map((img, index) => (
-          <img
-            src={img}
-            key={index}
-            alt={img}
-            onClick={() => changeImage(img)}
-          />
-        ))}
-      </div>
-      <div className="card-text">
-        <h2>{title}</h2>
-      </div>
+    <div ref={ref}>
+      <motion.div className="hsf-card" animate={animation}>
+        <img src={currentImg} alt={currentImg} />
+        <div className="images-card">
+          {images.map((img, index) => (
+            <img
+              src={img}
+              key={index}
+              alt={img}
+              onClick={() => changeImage(img)}
+            />
+          ))}
+        </div>
+        <div className="card-text">
+          <h2>{title}</h2>
+        </div>
+      </motion.div>
     </div>
   );
 };
