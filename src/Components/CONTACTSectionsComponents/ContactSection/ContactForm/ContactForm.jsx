1a3db454d2e55b54d76a7874afad1f91467c690c
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-04 13:20:15 +0100

    placeholder

--- src/Components/CONTACTSectionsComponents/ContactSection/ContactForm/ContactForm.jsx
+++ src/Components/CONTACTSectionsComponents/ContactSection/ContactForm/ContactForm.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const ContactForm = ({ sendEmail }) => {
   return (
     <form onSubmit={sendEmail}>
-      <input type="text" name="name" placeholder="ime" />
-      <input type="email" name="user_email" placeholder="email" />
+      <input type="text" name="name" placeholder="Vaše ime" />
+      <input type="email" name="user_email" placeholder="Vaša email adresa" />
       <textarea placeholder="Poruka..." name="message" />
       <button type="submit" value="Send">
         Slanje
