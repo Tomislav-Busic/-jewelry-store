115c1747c10ec638f10c5a74746d3bf018955885
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-06 19:21:54 +0100

    logout btn component

--- src/Components/LogoutBtn/LogoutBtn.jsx
+++ src/Components/LogoutBtn/LogoutBtn.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { loginActions } from "../../store/slice/login-slice";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "react-router-dom";
 
 export const LogoutBtn = () => {
-  return <div>LogoutBtn</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigation();
+
+  const handleLogout = () => {
+    dispatch(loginActions.logout());
+    navigate("/");
+  };
+
+  return <button onClick={handleLogout}>Odjavi se</button>;
 };
