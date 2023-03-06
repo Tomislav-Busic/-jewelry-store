e96376babac1c312508d027a9fd058b756384c6a
Tomislav Busic <tomislav.busic89@gmail.com>
2023-03-06 19:23:21 +0100

    logout btn component add scss

--- src/Components/LogoutBtn/LogoutBtn.jsx
+++ src/Components/LogoutBtn/LogoutBtn.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { loginActions } from "../../store/slice/login-slice";
 import { useDispatch } from "react-redux";
 import { useNavigation } from "react-router-dom";
+import "./LogoutBtn.scss";
 
 export const LogoutBtn = () => {
   const dispatch = useDispatch();
