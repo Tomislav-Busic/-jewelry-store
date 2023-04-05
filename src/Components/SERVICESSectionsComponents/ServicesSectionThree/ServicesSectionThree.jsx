1ca2a95c6edb282d962fef868a0778ac42081c20
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-05 19:16:44 +0200

    Services s3

--- src/Components/SERVICESSectionsComponents/ServicesSectionThree/ServicesSectionThree.jsx
+++ src/Components/SERVICESSectionsComponents/ServicesSectionThree/ServicesSectionThree.jsx
@@ -8,19 +8,13 @@ export const ServicesSectionThree = () => {
       <div className="left-s-side">
         <h2>Popravci i restauriranje nakita</h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          Sve vrste popravaka i restauracije nakita apsolutno su na našoj listi
+          prioriteta usluga koje nudimo kako bi smo udovoljili Vašim zahtjevima
+          i potrebama.
         </p>
       </div>
       <div className="right-s-side">
-        <img src={img} />
+        <img src={img} alt="img" />
       </div>
     </div>
   );
