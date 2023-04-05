02ab9414790b962d61e3d9199df4f89e552a8f35
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-05 19:11:47 +0200

    Services s2

--- src/Components/SERVICESSectionsComponents/ServicesSectionTwo/ServicesSectionTwo.jsx
+++ src/Components/SERVICESSectionsComponents/ServicesSectionTwo/ServicesSectionTwo.jsx
@@ -8,19 +8,14 @@ export const ServicesSectionTwo = () => {
       <div className="left-s-side">
         <h2>Izrada nakita po narudžbi</h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          Izradi iz majstorske draguljarske radionice <i>"Salzberger"</i>{" "}
+          spremni smo vaše želje i potrebe pretvoriti u vrhunske komade nakita.
+          Za Vas možemo dizajnirati ili po Vašem dizajnu ručno izraditi unikatne
+          primjerke nakita.
         </p>
       </div>
       <div className="right-s-side">
-        <img src={img} />
+        <img src={img} alt="img" />
       </div>
     </div>
   );
