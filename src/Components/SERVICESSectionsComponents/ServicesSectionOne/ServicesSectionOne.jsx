7453794ac01a8701b9652040e33855c494876273
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-05 18:59:10 +0200

    Services

--- src/Components/SERVICESSectionsComponents/ServicesSectionOne/ServicesSectionOne.jsx
+++ src/Components/SERVICESSectionsComponents/ServicesSectionOne/ServicesSectionOne.jsx
@@ -7,20 +7,18 @@ export const ServicesSectionOne = () => {
     <div className="services-s-one">
       <div className="left-s-side">
         <h2>Usluge</h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        <ul>
+          <li>Ručna izrada nakita po narudžbi</li>
+          <li>Procjena nakita</li>
+          <li>Nizanje bisera i koralja</li>
+          <li>Restauriranje antiknog namještaja</li>
+          <li>Tapeciranje namještaja</li>
+          <li>Procjena umjetnina</li>
+          <li>Restauriranje i čišćenje slika</li>
+        </ul>
       </div>
       <div className="right-s-side">
-        <img src={img} />
+        <img src={img} alt="img" />
       </div>
     </div>
   );
