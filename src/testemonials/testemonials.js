e7673aa678191e6ccee529e8aa9a56315d5d4c88
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-05 20:36:58 +0200

    testemonials

--- src/testemonials/testemonials.js
+++ src/testemonials/testemonials.js
@@ -6,12 +6,12 @@ export const testemonials = [
   },
   {
     content:
-      " Nedavno sam dala na popravak prsten u Calvi antique i usluga je bila izvanredna. Prsten je brzo i temeljito popravljen i izgleda kao nov. Impresionirana sam kvalitetom rada i toplo bih preporučila Calvi antique svakome tko traži popravak prstena za nakit.",
+      " Prsten je brzo i temeljito popravljen i izgleda kao nov. Impresionirana sam kvalitetom rada i toplo bih preporučila Calvi antique svakome tko traži popravak prstena za nakit.",
     name: "Vedrana S.",
   },
   {
     content:
-      " Nedavno sam dala popraviti ogrlicu u Calvi antique i bila sam jako zadovoljna rezultatima. Uspjeli su ga vratiti u prvobitno stanje i izgleda bolje nego ikada! Osoblje je bilo ljubazno, uslužno i vrlo obrazovano. Svakako bih preporučila Calvi antique za sve vaše potrebe popravka nakita.",
+      " Nedavno sam dala popraviti ogrlicu u Calvi antique i bila sam jako zadovoljna rezultatima. Uspjeli su je vratiti u prvobitno stanje i izgleda bolje nego ikada!",
     name: "Sanja T.",
   },
 ];
