6719a13812ef8d3c42e77271b9a03cd66729f299
Tomislav Busic <tomislav.busic89@gmail.com>
2023-04-06 21:49:48 +0200

    color

--- src/Components/SALESectionComponents/Products/Products.jsx
+++ src/Components/SALESectionComponents/Products/Products.jsx
@@ -25,7 +25,7 @@ export const Products = () => {
   return (
     <>
       <br />
-      <h2>Cijene su na upit</h2>
+      <h2 style={{ color: "#343434" }}>Cijene su na upit</h2>
       <div className="products">
         {products
           .filter(
