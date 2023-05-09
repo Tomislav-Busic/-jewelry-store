8f06321fcf80dddcfbbac110726de05dfd020226
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-09 20:12:40 +0200

    import img

--- src/Components/ModalImg/ModalImg.jsx
+++ src/Components/ModalImg/ModalImg.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./ModalImg.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { modalActions } from "../../store/slice/modal-slice";
 
 export const ModalImg = () => {
   const dispatch = useDispatch();
+  const src = useSelector((state) => state.modal.img);
 
   const toggleModal = () => {
     dispatch(modalActions.closeModal());
   };
 
-  return <div className="modal" onClick={() => toggleModal()}></div>;
+  return (
+    <div className="modal" onClick={() => toggleModal()}>
+      <img src={src} alt="img" />
+    </div>
+  );
 };
