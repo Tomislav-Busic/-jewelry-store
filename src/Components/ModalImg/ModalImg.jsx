dfa1ddfa1179ec22ed9a7677b8ca3cff095ae1e4
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-09 20:42:59 +0200

    setName

--- src/Components/ModalImg/ModalImg.jsx
+++ src/Components/ModalImg/ModalImg.jsx
@@ -6,6 +6,7 @@ import { modalActions } from "../../store/slice/modal-slice";
 export const ModalImg = () => {
   const dispatch = useDispatch();
   const src = useSelector((state) => state.modal.img);
+  const name = useSelector((state) => state.modal.name);
 
   const toggleModal = () => {
     dispatch(modalActions.closeModal());
@@ -13,7 +14,7 @@ export const ModalImg = () => {
 
   return (
     <div className="modal" onClick={() => toggleModal()}>
-      <h2>Title</h2>
+      <h2>{name}</h2>
       <img src={src} alt="img" />
     </div>
   );
