a7869c6171c442c9f0f6ce5f5270676ac68dc466
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-09 19:56:20 +0200

    import img modal

--- src/Pages/Sale/Sale.jsx
+++ src/Pages/Sale/Sale.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { CategoriesMenu } from "../../Components/SALESectionComponents/CategoriesMenu/CategoriesMenu";
 import { Filters } from "../../Components/SALESectionComponents/Filters/Filters";
 import { Products } from "../../Components/SALESectionComponents/Products/Products";
+import { ModalImg } from "../../Components/ModalImg/ModalImg";
+import { useSelector } from "react-redux";
 import "./Sale.scss";
 
 export const Sale = () => {
+  const imgModal = useSelector((state) => state.modal.toggleModal);
   return (
     <div className="art-page">
       <div className="container">
         <CategoriesMenu />
         <Filters />
         <Products />
+        {imgModal && (
+          <>
+            <ModalImg />
+          </>
+        )}
       </div>
     </div>
   );
