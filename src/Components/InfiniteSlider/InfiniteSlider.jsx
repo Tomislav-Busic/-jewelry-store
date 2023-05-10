a5127be239771e2858a98f3ebbc17ac6fb7e1fa8
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-10 17:49:21 +0200

    showProduct

--- src/Components/InfiniteSlider/InfiniteSlider.jsx
+++ src/Components/InfiniteSlider/InfiniteSlider.jsx
@@ -5,19 +5,23 @@ import { useSelector } from "react-redux";
 export const InfiniteSlider = () => {
   const images = useSelector((state) => state.data.dataList);
 
+  const showProduct (img) => {
+
+  }
+
   return (
     <div className="slider-component">
       <div className="slider">
         <div className="slide-track">
           {images.map((img, index) => (
-            <div className="slide" key={index}>
+            <div onClick={() => showProduct(img.img)} className="slide" key={index}>
               <img src={img.img} alt="img" />
             </div>
           ))}
         </div>
         <div className="slide-track two">
           {images.map((img, index) => (
-            <div className="slide" key={index}>
+            <div onClick={() => showProduct(img.img)} className="slide" key={index}>
               <img src={img.img} alt="img" />
             </div>
           ))}
