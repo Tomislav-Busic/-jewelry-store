3508f0923a290293a00e9ec62b1fee3332d7209e
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-10 17:57:32 +0200

    showProduct

--- src/Components/InfiniteSlider/InfiniteSlider.jsx
+++ src/Components/InfiniteSlider/InfiniteSlider.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import "./InfiniteSlider.scss";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { modalActions } from "../../store/slice/modal-slice";
 
 export const InfiniteSlider = () => {
   const images = useSelector((state) => state.data.dataList);
+  const dispatch = useDispatch();
 
-  const showProduct (img) => {
-
-  }
+  const showProduct = (item) => {
+    const { name, img } = item;
+    dispatch(modalActions.setName(name));
+    dispatch(modalActions.setImg(img));
+    dispatch(modalActions.closeModal());
+  };
 
   return (
     <div className="slider-component">
       <div className="slider">
         <div className="slide-track">
           {images.map((img, index) => (
-            <div onClick={() => showProduct(img.img)} className="slide" key={index}>
+            <div onClick={() => showProduct(img)} className="slide" key={index}>
               <img src={img.img} alt="img" />
             </div>
           ))}
         </div>
         <div className="slide-track two">
           {images.map((img, index) => (
-            <div onClick={() => showProduct(img.img)} className="slide" key={index}>
+            <div onClick={() => showProduct(img)} className="slide" key={index}>
               <img src={img.img} alt="img" />
             </div>
           ))}
