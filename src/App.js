344bd93974d82f9c8994ac9291793276aafa7836
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-29 11:00:20 +0200

    arhitecture

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { loginActions } from "./store/slice/login-slice";
-import { firebaseData } from "./FirebaseDB/firebaseData";
+import { firebaseData } from "./firebaseDatairebaseDB/firebaseData";
 
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Footer } from "./Components/Footer/Footer";
