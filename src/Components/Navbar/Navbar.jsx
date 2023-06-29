5426077a7f94d5ad37ae9912844503ebaf924342
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-29 18:16:17 +0200

    arhitecure and clean code

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,59 +1,65 @@
 import React, { useState } from "react";
-import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { goToTopOfPage } from "../../utilities/tools";
+
 import logo from "../../images/calvi.png";
+
+import styles from "./navbar.module.scss";
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <div className="navbar">
+    <div className={styles.navbar}>
       <header>
-        <Link className="logo-name" to="/" onClick={() => goToTopOfPage()}>
+        <Link
+          className={styles.logo_name}
+          to="/"
+          onClick={() => goToTopOfPage()}
+        >
           <h3>CALVI</h3>
         </Link>
-        <nav className={toggle ? "resposive_nav" : ""}>
+        <nav className={toggle ? `${styles.resposive_nav}` : ""}>
           <Link
-            className="nav-link"
+            className={styles.nav_link}
             to="/"
             onClick={() => (setToggle(!toggle), goToTopOfPage())}
           >
             POČETNA
           </Link>
           <Link
-            className="nav-link"
+            className={styles.nav_link}
             to="/prodaja"
             onClick={() => (setToggle(!toggle), goToTopOfPage())}
           >
             PRODAJA
           </Link>
           <Link
-            className="nav-link"
+            className={styles.nav_link}
             to="/usluge "
             onClick={() => (setToggle(!toggle), goToTopOfPage())}
           >
             USLUGE
           </Link>
           <Link
-            className="nav-link"
+            className={styles.nav_link}
             to="/kontakt"
             onClick={() => (setToggle(!toggle), goToTopOfPage())}
           >
             KONTAKT
           </Link>
           <button
-            className="nav-btn nav-close-btn"
+            className={styles.nav_btn && styles.nav_close_btn}
             onClick={() => setToggle(!toggle)}
           >
             <FaTimes />
           </button>
         </nav>
-        <button className="nav-btn" onClick={() => setToggle(!toggle)}>
+        <button className={styles.nav_btn} onClick={() => setToggle(!toggle)}>
           <FaBars />
         </button>
-        <div className="logo">
+        <div className={styles.logo}>
           <img src={logo} alt="logo" />
         </div>
       </header>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
