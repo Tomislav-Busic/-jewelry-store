f03cfe88610bd843f25403578569f31796f37f7d
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-30 10:56:19 +0200

    ContactLinks

--- src/Components/contact/contactLinks/ContactLinks.jsx
+++ src/Components/contact/contactLinks/ContactLinks.jsx
@@ -1,37 +1,40 @@
 import React from "react";
-import "./contactLinks.scss";
+
+import { SocialIcons } from "components/socialIcons/SocialIcons";
+
 import { IoMail, IoCall } from "react-icons/io5";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import { SocialIcons } from "../../../socialIcons/SocialIcons";
+
+import styles from "./contactLinks.module.scss";
 
 export const ContactLinks = () => {
   return (
-    <div className="contact-links">
+    <div className={styles.contact_links}>
       {" "}
       <SocialIcons />
-      <div className="f-con-item">
+      <div className={styles.f_con_item}>
         <ul>
           <li>
-            <IoMail className="icon" />
-            <a href="mailto:anita@calviantique.hr" className="link">
+            <IoMail className={styles.icon} />
+            <a href="mailto:anita@calviantique.hr" className={styles.link}>
               anita@calviantique.hr
             </a>
           </li>
           <li>
-            <IoCall className="icon" />
-            <a href="tel:+385992882894" className="link">
+            <IoCall className={styles.icon} />
+            <a href="tel:+385992882894" className={styles.link}>
               <span>Anita</span> +385992882894
             </a>
           </li>
           <li>
-            <IoCall className="icon" />
-            <a href="tel:+38598229078" className="link">
+            <IoCall className={styles.icon} />
+            <a href="tel:+38598229078" className={styles.link}>
               <span>Ivica</span> +38598229078
             </a>
           </li>
           <li>
-            <FaMapMarkerAlt className="icon" />
-            <p className="link">Praška 10, Zagreb</p>
+            <FaMapMarkerAlt className={styles.icon} />
+            <p className={styles.link}>Praška 10, Zagreb</p>
           </li>
         </ul>
       </div>
