fd63c7d65bd6139da7a1281b104cf1e34516831f
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-30 16:24:54 +0200

    Login

--- src/Pages/loginPage/LoginPage.jsx
+++ src/Pages/loginPage/LoginPage.jsx
@@ -1,73 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebase";
-import { loginActions } from "store/slice/login-slice";
-import { goToTopOfPage } from "utilities/tools";
-import { LoginForm } from "components/adminComponents/loginForm/LoginForm";
+import { Login } from "features/login/Login";
 
-import styles from "./login.module.scss";
+import styles from "./loginPage.module.scss";
 
 export const LoginPage = () => {
-  const [error, setError] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(isLoggedIn));
-  }, [isLoggedIn]);
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        dispatch(loginActions.login(user));
-        navigate("/admin-dashboard");
-        goToTopOfPage();
-      })
-      .catch((error) => {
-        setError(true);
-        console.log(error);
-      });
-  };
-
-  const goToDashboard = () => {
-    navigate("/admin-dashboard");
-    goToTopOfPage();
-  };
-
-  const handleBack = () => {
-    dispatch(loginActions.logout());
-    navigate("/");
-    goToTopOfPage();
-  };
-
   return (
     <div className={styles.loggin}>
-      <h1>Administrator</h1>
-
-      <div className={styles.container}>
-        <h2>
-          Ukoliko niste vlasnik stranice molimo Vas da se vratite na početnu
-          stranicu! Hvala :)
-        </h2>
-        <button onClick={handleBack}>Povratak na početnu</button>
-        <LoginForm
-          handleLogin={handleLogin}
-          isLoggedIn={isLoggedIn}
-          goToDashboard={goToDashboard}
-          setEmail={setEmail}
-          setPassword={setPassword}
-          error={error}
-        />
-      </div>
+      <Login />
     </div>
   );
 };
---
+++ src/features/login/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { loginActions } from "store/slice/login-slice";
+import { goToTopOfPage } from "utilities/tools";
+import { LoginForm } from "./components/loginForm/LoginForm";
+
+import styles from "./login.module.scss";
+
+export const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        dispatch(loginActions.login(user));
+        navigate("/admin-dashboard");
+        goToTopOfPage();
+      })
+      .catch((error) => {
+        setError(true);
+        console.log(error);
+      });
+  };
+
+  const goToDashboard = () => {
+    navigate("/admin-dashboard");
+    goToTopOfPage();
+  };
+
+  const handleBack = () => {
+    dispatch(loginActions.logout());
+    navigate("/");
+    goToTopOfPage();
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Administrator</h1>
+      <h2>
+        Ukoliko niste vlasnik stranice molimo Vas da se vratite na početnu
+        stranicu! Hvala :)
+      </h2>
+      <button onClick={handleBack}>Povratak na početnu</button>
+      <LoginForm
+        handleLogin={handleLogin}
+        isLoggedIn={isLoggedIn}
+        goToDashboard={goToDashboard}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        error={error}
+      />
+    </div>
+  );
+};
