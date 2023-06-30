0dde9103e3d12af55401ee703c542d5358e1bbc4
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-30 11:32:06 +0200

    HomeSectionOne

--- src/Components/HOMESectionsComponents/HomeSectionOne/HomeSectionOne.jsx
+++ src/Components/HOMESectionsComponents/HomeSectionOne/HomeSectionOne.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { TextAnimation } from "../../textAnimation/TextAnimation";
-import { AnimationBtn } from "../../animationBtn/AnimationBtn";
-import { SocialIcons } from "../../socialIcons/SocialIcons";
-import "./homeSectionOne.scss";
+
+import { TextAnimation } from "components/textAnimation/TextAnimation";
+import { AnimationBtn } from "components/animationBtn/AnimationBtn";
+import { SocialIcons } from "components/socialIcons/SocialIcons";
+
+import styles from "./homeSectionOne.module.scss";
 
 export const HomeSectionOne = () => {
   return (
-    <div className="home-section-one">
+    <div className={styles.home_section_one}>
       <h1>
         <span>CALVI </span>antique fine jewellery
       </h1>
       <TextAnimation />
       <SocialIcons />
       <h3>Praška 10, Zagreb</h3>
-      <AnimationBtn
-        name={"Kontakt"}
-        path={"./kontakt"}
-        color={"btn-anim-red"}
-      />
+      <AnimationBtn name={"Kontakt"} path={"/kontakt"} color={"btn-anim-red"} />
     </div>
   );
 };
