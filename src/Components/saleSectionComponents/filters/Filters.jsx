a6ae42e2f9ea7779d806afbfdf4dd6de634d58b1
Tomislav Busic <tomislav.busic89@gmail.com>
2023-06-30 12:03:06 +0200

    Filters

--- src/Components/saleSectionComponents/filters/Filters.jsx
+++ src/Components/saleSectionComponents/filters/Filters.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import "./filters.scss";
 import { useDispatch } from "react-redux";
-import { dataActions } from "../../../store/slice/data-slice";
+import { dataActions } from "store/slice/data-slice";
+
+import styles from "./filters.module.scss";
 
 export const Filters = () => {
   const dispatch = useDispatch();
@@ -10,24 +11,12 @@ export const Filters = () => {
     dispatch(dataActions.filterByName(name));
   };
 
-  /* const handleSortByPrice = (value) => {
-    dispatch(dataActions.sortByPrice(value));
-  }; */
-
   return (
-    <div className="filters">
+    <div className={styles.filters}>
       <input
         placeholder="Upiši ime proizvoda"
         onChange={(e) => handleSearchByName(e.target.value)}
       />
-      {/* <select
-        name="Filtriraj po cijeni"
-        onChange={(e) => handleSortByPrice(e.target.value)}
-      >
-        <option>Sortiraj po cijeni</option>
-        <option value="lower">Niža cijena</option>
-        <option value="higher">Viša cijena</option>
-      </select> */}
     </div>
   );
 };
