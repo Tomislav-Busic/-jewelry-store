31072a68a25478497b3dd813d9b7be2ac029ff5d
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-02 15:36:22 +0200

    path

--- src/store/slice/data/data-service.js
+++ src/store/slice/data/data-service.js
@@ -1,5 +1,5 @@
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../../firebaseDB/firebase";
+import { db } from "firebaseDB/firebase";
 
 export const firebaseData = async () => {
   let list = [];
