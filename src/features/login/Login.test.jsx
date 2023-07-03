59bfa033a4d0456913ece7d83fa68500eda07b4b
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-03 23:54:18 +0200

    test Login

--- src/features/login/Login.test.jsx
+++ src/features/login/Login.test.jsx
@@ -1,10 +1,19 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "store";
 import { Login } from "./Login";
 
 describe("Login", () => {
   it("should render Login", () => {
-    const { getByText } = render(<Login />);
+    const { getByText } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          {" "}
+          <Login />
+        </MemoryRouter>
+      </Provider>
+    );
 
     expect(getByText).not.toBeNull();
   });
