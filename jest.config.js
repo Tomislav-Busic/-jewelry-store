a2b6b576ba6f559c4f21c3c3699e8f9a3cc34016
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-05 22:56:02 +0200

    new setup

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,11 @@
-const config = {
-  roots: ["<rootDir>/test", "<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.css$": "<rootDir>/jest-config/style-mock.js",
-  },
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testEnvironment: "jsdom",
+module.exports = {
   collectCoverage: false,
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "^.+\\.(css|sass|scss)$": "identity-obj-proxy",
   },
 };
-
-export default config;
