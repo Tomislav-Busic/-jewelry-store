b92c32db73f02210122e9081ceef77b99873e122
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-06 18:50:36 +0200

    Navbar test

---
+++ src/Components/Navbar/Navbar.test.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+import { Navbar } from "./Navbar";
+
+describe("Navbar component", () => {
+  it("should render correctly", () => {
+    const { container } = render(<Navbar />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should toggle the navbar when clicking on the hamburger icon", () => {
+    render(<Navbar />);
+
+    expect(screen.getByTestId("navbar")).not.toHaveClass("resposive_nav");
+
+    fireEvent.click(screen.getByTestId("hamburger-icon"));
+
+    expect(screen.getByTestId("navbar")).toHaveClass("resposive_nav");
+  });
+
+  it("should close the navbar when clicking on the close icon", () => {
+    render(<Navbar />);
+
+    fireEvent.click(screen.getByTestId("hamburger-icon"));
+
+    expect(screen.getByTestId("navbar")).toHaveClass("resposive_nav");
+
+    fireEvent.click(screen.getByTestId("close-icon"));
+
+    expect(screen.getByTestId("navbar")).not.toHaveClass("resposive_nav");
+  });
+});
