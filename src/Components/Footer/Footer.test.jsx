ec0be08015dd087c310395ef9dd3a85704a2e3da
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-06 16:10:46 +0200

    tests Footer

--- src/Components/Footer/Footer.test.jsx
+++ src/Components/Footer/Footer.test.jsx
@@ -2,3 +2,25 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
+import { Footer } from "./Footer";
+
+describe("Footer", () => {
+  it("should render the correct year", () => {
+    render(<Footer />);
+
+    const currentYear = new Date().getFullYear();
+
+    expect(
+      screen.getByText(`© 2023 - ${currentYear} CALVI`)
+    ).toBeInTheDocument();
+  });
+
+  it("should render the correct link", () => {
+    render(<Footer />);
+
+    expect(screen.getByText("Tomislav Bušić")).toHaveAttribute(
+      "href",
+      "https://tomislav-busic.web.app/"
+    );
+  });
+});
