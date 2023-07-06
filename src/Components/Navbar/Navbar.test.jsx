75865e62c3ffce2fe2177379e64f520c1222e3d1
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-06 21:59:37 +0200

    Navbar test

--- src/Components/Navbar/Navbar.test.jsx
+++ src/Components/Navbar/Navbar.test.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import user from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-import { createMemoryHistory } from "history";
 import { MemoryRouter } from "react-router-dom";
 import { Navbar } from "./Navbar";
+
+const renderWithRouter = (ui, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+
+  return {
+    user: userEvent.setup(),
+    ...render(ui, { wrapper: MemoryRouter }),
+  };
+};
 
 describe("Navbar component", () => {
   it("should render correctly", () => {
@@ -24,24 +32,17 @@ describe("Navbar component", () => {
       </MemoryRouter>
     );
 
-    user.click(screen.getByTestId("nav_header"));
+    userEvent.click(screen.getByTestId("nav_header"));
 
     expect(window.location.pathname).toBe("/");
   });
 
   it("should navigate to the prodaja page when clicking on the prodaja link", async () => {
-    const history = createMemoryHistory();
-    render(
-      <MemoryRouter history={history}>
-        <Navbar />
-      </MemoryRouter>
-    );
+    const { user } = renderWithRouter(<Navbar />);
 
     await user.click(screen.getByText("PRODAJA"));
 
-    expect(
-      await waitFor(() => screen.findByText("CIJENE SU NA UPIT"))
-    ).toBeInTheDocument();
+    expect(screen.getByText("CIJENE SU NA UPIT")).toBeInTheDocument();
   });
 
   it("should navigate to the usluge page when clicking on the usluge link", () => {
@@ -51,7 +52,7 @@ describe("Navbar component", () => {
       </MemoryRouter>
     );
 
-    user.click(screen.getByText("USLUGE"));
+    userEvent.click(screen.getByText("USLUGE"));
 
     expect(window.location.pathname).toBe("usluge");
   });
@@ -63,7 +64,7 @@ describe("Navbar component", () => {
       </MemoryRouter>
     );
 
-    user.click(screen.getByText("KONTAKT"));
+    userEvent.click(screen.getByText("KONTAKT"));
 
     expect(window.location.pathname).toBeCalledWith("/kontakt");
   });
