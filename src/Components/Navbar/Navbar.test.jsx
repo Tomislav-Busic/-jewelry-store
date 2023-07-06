e517a3030d956bee82458e193b8d58392c061389
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-06 22:02:28 +0200

    Navbar test

--- src/Components/Navbar/Navbar.test.jsx
+++ src/Components/Navbar/Navbar.test.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 import { Navbar } from "./Navbar";
-
-const renderWithRouter = (ui, { route = "/" } = {}) => {
-  window.history.pushState({}, "Test page", route);
-
-  return {
-    user: userEvent.setup(),
-    ...render(ui, { wrapper: MemoryRouter }),
-  };
-};
 
 describe("Navbar component", () => {
   it("should render correctly", () => {
@@ -35,38 +25,6 @@ describe("Navbar component", () => {
     userEvent.click(screen.getByTestId("nav_header"));
 
     expect(window.location.pathname).toBe("/");
-  });
-
-  it("should navigate to the prodaja page when clicking on the prodaja link", async () => {
-    const { user } = renderWithRouter(<Navbar />);
-
-    await user.click(screen.getByText("PRODAJA"));
-
-    expect(screen.getByText("CIJENE SU NA UPIT")).toBeInTheDocument();
-  });
-
-  it("should navigate to the usluge page when clicking on the usluge link", () => {
-    render(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
-    );
-
-    userEvent.click(screen.getByText("USLUGE"));
-
-    expect(window.location.pathname).toBe("usluge");
-  });
-
-  it("should navigate to the kontakt page when clicking on the kontakt link", () => {
-    render(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
-    );
-
-    userEvent.click(screen.getByText("KONTAKT"));
-
-    expect(window.location.pathname).toBeCalledWith("/kontakt");
   });
 
   it("should toggle the navbar when clicking on the hamburger icon", () => {
