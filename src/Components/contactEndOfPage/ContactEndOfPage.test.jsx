c661feb4fb25d5919a65a1d267b183d37bd82746
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-06 15:45:54 +0200

    tests ContactEndOfPage

---
+++ src/Components/contactEndOfPage/ContactEndOfPage.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+import { ContactEndOfPage } from "./ContactEndOfPage";
+
+describe("ContactEndOfPage", () => {
+  it("should render the component correctly", () => {
+    render(<ContactEndOfPage />);
+
+    expect(screen.getByTestId("footer_contact")).toBeInTheDocument();
+    expect(screen.getByTestId("footer_contact_container")).toBeInTheDocument();
+    expect(screen.getByTestId("f_con_item")).toBeInTheDocument();
+    expect(screen.getByTestId("map")).toBeInTheDocument();
+  });
+
+  it("should render the logo correctly", () => {
+    render(<ContactEndOfPage />);
+
+    expect(screen.getByAltText("logo")).toBeInTheDocument();
+  });
+
+  it("should render the social icons correctly", () => {
+    render(<ContactEndOfPage />);
+
+    expect(screen.getByTestId("social_icons")).toBeInTheDocument();
+  });
+});
