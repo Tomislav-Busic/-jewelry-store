8474303018e9a71a86f0e336eec3f318216f73b0
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-07 18:07:11 +0200

    test ListOfProductsAdmin

--- src/features/components/listOfProductsAdmin/ListOfProductsAdmin.test.jsx
+++ src/features/components/listOfProductsAdmin/ListOfProductsAdmin.test.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "store";
 import { ListOfProductsAdmin } from "./ListOfProductsAdmin";
+import { ProductAdmin } from "./productAdmin/ProductAdmin";
 
 describe("ListOfProductsAdmin", () => {
   it("should render correctly", () => {
-    const { container } = render(<ListOfProductsAdmin />);
-    expect(container).toMatchSnapshot();
+    const { container } = render(
+      <MemoryRouter initialEntries={["/admin-dashboard"]}>
+        <Provider store={store}>
+          <ListOfProductsAdmin />
+        </Provider>
+      </MemoryRouter>
+    );
+    expect(container).not.toBeNull();
+  });
+
+  it("should render the table with the correct data", () => {
+    const mockData = [
+      { name: "Product 1", category: "Category 1" },
+      { name: "Product 2", category: "Category 2" },
+    ];
+    const view = render(
+      <MemoryRouter initialEntries={["/admin-dashboard"]}>
+        <Provider store={store}>
+          <ListOfProductsAdmin data={mockData} />
+        </Provider>
+      </MemoryRouter>
+    );
+
+    expect(view.find("table").length).toBe(1);
+    expect(view.find("tr").length).toBe(3);
   });
 });
