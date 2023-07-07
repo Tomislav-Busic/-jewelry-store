351a3a8791b0c21ba5abbe10973a4ea954c8cfeb
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-07 18:08:57 +0200

    test ListOfProductsAdmin

--- src/features/components/listOfProductsAdmin/ListOfProductsAdmin.test.jsx
+++ src/features/components/listOfProductsAdmin/ListOfProductsAdmin.test.jsx
@@ -18,21 +18,4 @@ describe("ListOfProductsAdmin", () => {
     );
     expect(container).not.toBeNull();
   });
-
-  it("should render the table with the correct data", () => {
-    const mockData = [
-      { name: "Product 1", category: "Category 1" },
-      { name: "Product 2", category: "Category 2" },
-    ];
-    const view = render(
-      <MemoryRouter initialEntries={["/admin-dashboard"]}>
-        <Provider store={store}>
-          <ListOfProductsAdmin data={mockData} />
-        </Provider>
-      </MemoryRouter>
-    );
-
-    expect(view.find("table").length).toBe(1);
-    expect(view.find("tr").length).toBe(3);
-  });
 });
