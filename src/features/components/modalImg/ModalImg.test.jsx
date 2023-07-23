067cc01d1921b1ff07a036a29e65c09546c42077
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-23 23:25:06 +0200

    ModalImg test

---
+++ src/features/components/modalImg/ModalImg.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useSelector, useDispatch } from "react-redux";
+import { ModalImg } from "./ModalImg";
+import { modalActions } from "store/slice/modal/modal-slice";
+
+jest.mock("react-redux", () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
+
+jest.mock("store/slice/modal/modal-slice", () => ({
+  modalActions: {
+    closeModal: jest.fn(),
+  },
+}));
+
+describe("ModalImg", () => {
+  const dispatch = jest.fn();
+  useDispatch.mockReturnValue(dispatch);
+
+  test("renders modal with name and image", () => {
+    useSelector.mockReturnValueOnce("image-url");
+    useSelector.mockReturnValueOnce("Image Name");
+
+    render(<ModalImg />);
+
+    expect(screen.getByText("Image Name")).toBeInTheDocument();
+    expect(screen.getByAltText("img")).toHaveAttribute("src", "image-url");
+  });
+});
