6c3c51e54f281740588835f8a1e41a7de28b4d14
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-24 21:03:44 +0200

    Contact test

--- src/features/contact/Contact.test.jsx
+++ src/features/contact/Contact.test.jsx
@@ -1,10 +1,12 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import { Contact } from "./Contact";
 
 describe("Contact", () => {
   it("should render Contact component", () => {
     render(<Contact />);
-    const heading = screen.getByText("Kontakt");
+
+    const heading = screen.getByText(/Kontakt/i);
     const form = screen.getByTestId("form");
     const links = screen.getByTestId("links");
 
@@ -12,4 +14,17 @@ describe("Contact", () => {
     expect(form).toBeInTheDocument();
     expect(links).toBeInTheDocument();
   });
+
+  /*
+    it("should trigger sendEmail function on form submit", () => {
+    const { getByLabelText } = render(<Contact />);
+    const form = getByLabelText("contact-form");
+    const preventDefault = jest.fn();
+    const mockSendEmail = jest.fn();
+
+    fireEvent.submit(form, { preventDefault });
+    expect(preventDefault).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+  });
+  */
 });
