9efb2878a5b363ecfa72c5b6ed764689184d8b6e
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-25 15:21:07 +0200

    ContactLinks test

--- src/features/contact/components/contactLinks/ContactLinks.test.jsx
+++ src/features/contact/components/contactLinks/ContactLinks.test.jsx
@@ -8,4 +8,29 @@ describe("ContactLinks", () => {
     expect(screen.getByTestId("contact_links")).toBeInTheDocument();
     expect(screen.getByTestId("social_icons")).toBeInTheDocument();
   });
+
+  it("renders the contact information", () => {
+    render(<ContactLinks />);
+    expect(screen.getByText("anita@calviantique.hr")).toBeInTheDocument();
+    expect(screen.getByText("anita@calviantique.hr")).toHaveAttribute(
+      "href",
+      "mailto:anita@calviantique.hr"
+    );
+
+    expect(screen.getByText("Anita")).toBeInTheDocument();
+    expect(screen.getByText("+385992882894")).toBeInTheDocument();
+    expect(screen.getByText("+385992882894")).toHaveAttribute(
+      "href",
+      "tel:+385992882894"
+    );
+
+    expect(screen.getByText("Ivica")).toBeInTheDocument();
+    expect(screen.getByText("+38598229078")).toBeInTheDocument();
+    expect(screen.getByText("+38598229078")).toHaveAttribute(
+      "href",
+      "tel:+38598229078"
+    );
+
+    expect(screen.getByText("Praška 10, Zagreb")).toBeInTheDocument();
+  });
 });
