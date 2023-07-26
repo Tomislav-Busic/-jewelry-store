556e21844938d8db2b22aaf1c9320e456b293a33
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-26 16:54:10 +0200

    PostProductForm test

---
+++ src/features/dashboard/postProductForm/PostProductForm.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { PostProductForm } from "./PostProductForm";
+
+describe("PostProductForm", () => {
+  const handleSubmit = jest.fn();
+  const handleData = jest.fn();
+  const setFile = jest.fn();
+  const file = null;
+  const perc = null;
+
+  test("renders form", () => {
+    render(
+      <PostProductForm
+        handleSubmit={handleSubmit}
+        handleData={handleData}
+        setFile={setFile}
+        file={file}
+        perc={perc}
+      />
+    );
+
+    expect(screen.getByText("Unesite novi proizvod")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Ime proizvoda")).toBeInTheDocument();
+    expect(screen.getByLabelText("Učitaj sliku:")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Potvrdi" })).toBeInTheDocument();
+  });
+});
