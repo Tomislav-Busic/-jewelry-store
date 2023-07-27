93d4e08b944950b64572c1805e668d36bff6d493
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-27 19:27:29 +0200

    Home test

---
+++ src/features/home/Home.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Provider } from "react-redux";
+import { store } from "store";
+import { MemoryRouter } from "react-router-dom";
+import { Home } from "./Home";
+
+describe("Dashboard", () => {
+  test("should render Dashboard component", () => {
+    render(<Home />);
+  });
+});
