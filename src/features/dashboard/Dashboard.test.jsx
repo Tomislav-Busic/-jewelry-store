6fe5866333795ad66c528a72b95be0ea4576f947
Tomislav Busic <tomislav.busic89@gmail.com>
2023-07-27 18:50:31 +0200

    Dashboard

--- src/features/dashboard/Dashboard.test.jsx
+++ src/features/dashboard/Dashboard.test.jsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { Provider } from "react-redux";
+import { store } from "store";
 import { Dashboard } from "./Dashboard";
 
 describe("Dashboard", () => {
   test("should render Dashboard component", () => {
-    render(<Dashboard />);
+    render(
+      <Provider store={store}>
+        <Dashboard />
+      </Provider>
+    );
 
     expect(screen.getByText(/Administrator/i)).toBeInTheDocument();
   });
